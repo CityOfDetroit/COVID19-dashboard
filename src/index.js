cf6ca27a6fe92817835f307caf2aef9f1908e9c2
Edgar Montes <jedgar1mx@gmail.com>
2020-03-18 13:56:37 -0400

    Loading state data.

--- src/index.js
+++ src/index.js
@@ -4,48 +4,18 @@ import './index.scss';
     fetch('https://us-central1-detroit-iet.cloudfunctions.net/getToken')
     .then((resp) => resp.json()) // Transform the data into json
     .then(function(token) {
-        let url = `https://services2.arcgis.com/qvkbeam7Wirps6zC/arcgis/rest/services/Health_Data/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnHiddenFields=false&returnGeometry=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token=${token.access_token}`;
+        let url = `https://services1.arcgis.com/0MSEUqKaxRlEPj5g/ArcGIS/rest/services/Coronavirus_2019_nCoV_Cases/FeatureServer/1/query?where=Country_Region+%3D%27US%27+AND+Province_State+%3D+%27Michigan%27&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=true&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=json&token=`;
         fetch(url)
         .then((resp) => resp.json()) // Transform the data into json
         .then(function(data) {
-            let detected = 0;
-            let quarantined = 0;
-            let recovered = 0;
-            let deaths = 0;
-            if(data.features.length){
-                data.features.forEach(element => {
-                    switch (true) {
-                        case element.attributes.Quarantine == '1':
-                            detected++;
-                            quarantined++;
-                            break;
-
-                        case element.attributes.Recovery == '1':
-                            detected++;
-                            recovered++;
-                            break;
-
-                            case element.attributes.Death == '1':
-                            detected++;
-                            recovered++;
-                            break;
-                    
-                        default:
-                            break;
-                    }
-                });
-            }
-            document.querySelector('.data-block.detected span').innerHTML = detected;
-            document.querySelector('.data-block.quarantined span').innerHTML = quarantined;
-            document.querySelector('.data-block.recovered span').innerHTML = recovered;
-            document.querySelector('.data-block.deaths span').innerHTML = deaths;
+            document.querySelector('.data-block.detected-mich span').innerHTML = data.features[0].attributes.Confirmed;
             let today = new Date();
             let dd = String(today.getDate()).padStart(2, '0');
             let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
             let yyyy = today.getFullYear();
             let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
             today = `${mm}/${dd}/${yyyy}`;
-            document.querySelector('.dashboard .update i').innerHTML = `Last updated ${today} - ${time}`;
+            document.querySelector('.update-date i').innerHTML = `Last updated ${today} - ${time}`;
             document.querySelector('#loader-overlay').className = '';
         });
     });    
